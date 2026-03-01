dea765e5678d7d6fad4f6c6fb7d6ffd614404b75
Pibody12 <135586624+Pibody12@users.noreply.github.com>
2026-03-01 10:37:20 +0300

    правки по ревью

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -14,7 +14,7 @@ import static tests.testData.TestData.*;
 public class TextBoxTests {
 
     @BeforeAll
-    static void openBrowser() {
+    static void setupSelenideConfig() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
