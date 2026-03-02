dd68010aa115872b0e4405bb6b9aca18b479977d
Timofei Drobotenko <timon.atm@gmail.com>
2026-03-02 21:32:20 +0300

    добавил Pages

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -2,55 +2,70 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import tests.testData.TestData;
+import pages.TextBoxPage;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static tests.testData.TestData.*;
 
 public class TextBoxTests {
+    TextBoxPage textBoxPage = new TextBoxPage();
 
     @BeforeAll
-    static void openBrowser() {
+    static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
-    void fillFullFormTest() {
-        open("");
-        executeJavaScript("""
-        document.getElementById('fixedban')?.remove();
-        document.querySelector('footer')?.remove();
-        """);
-        $$(".card-body").findBy(text("Elements")).click();
-        $$(".router-link").findBy(text("Text Box")).click();
-        $("#userName").setValue(userFirstName + " " + userLastName);
-        $("#userEmail").setValue(userEmail);
-        $("#currentAddress").setValue(currentAddress);
-        $("#permanentAddress").setValue(permanentAddress);
-        $("#submit").click();
+    void fillFormTest() {
+        textBoxPage.openPage()
+                .typeUserName(userName)
+                .typeUserEmail(userEmail)
+                .typeCurrentAddress(currentAddress)
+                .typePermanentAddress(permanentAddress)
+                .submitButtonClick()
 
-        $("#output #name").shouldHave(text(userFirstName + " " + userLastName));
-        $("#output #email").shouldHave(text(userEmail));
-        $("#output #currentAddress").shouldHave(text(currentAddress));
-        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+                .checkField("name", userName)
+                .checkField("email", userEmail)
+                .checkField("currentAddress", currentAddress)
+                .checkField("permanentAddress", permanentAddress);
     }
 
     @Test
     void fillFormWithoutAddressTest() {
-        open("");
-        $$(".card-body").findBy(text("Elements")).click();
-        $$(".router-link").findBy(text("Text Box")).click();
-        $("#userName").setValue(userFirstName + " " + userLastName);
-        $("#userEmail").setValue(userEmail);
+        textBoxPage.openPage();
+        textBoxPage.typeUserName(userName);
+        textBoxPage.typeUserEmail(userEmail);
+        textBoxPage.submitButtonClick();
+
+        textBoxPage.checkField("name", userName);
+        textBoxPage.checkField("email", userEmail);
+    }
+
+    @Test
+    void fillFormWithoutAddressTest_dsl() {
+        textBoxPage.openPage()
+                .typeUserName(userName)
+                .typeUserEmail(userEmail)
+                .submitButtonClick()
+
+                .checkField("name", userName)
+                .checkField("email", userEmail);
+    }
+
+
+    @Test
+    void fillFormWithoutAddressTest_old() {
+        open("/text-box");
+        $("#userName").setValue("Tim Drob");
+        $("#userEmail").setValue("tim@test.ru");
         $("#submit").click();
 
-        $("#output #name").shouldHave(text(userFirstName + " " + userLastName));
-        $("#output #email").shouldHave(text(userEmail));
+        $("#output #name").shouldHave(text("Tim"));
+        $("#output #email").shouldHave(text("tim@test.ru"));
     }
 }
